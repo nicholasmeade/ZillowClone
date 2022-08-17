a10c8a46b442001f7dfe06feb1cf5c372fd4108a
Nicholas Meade <nicholasjjmeade@gmail.com>
2022-08-17 19:18:54 -0400

    added background image for top part of home page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import React from "react";
 import { Nav, NavItem, NavLink } from "reactstrap";
 import logo from "./Assets/logo.png";
+import house from "./Assets/house.jpg";
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
       <ul className="nav-container">
         <li>
           <Nav className="me-2" fill justified>
@@ -70,42 +72,7 @@ function App() {
           </Nav>
         </li>
       </ul>
-      {/* <Nav
-      className="navbar" 
-      fill 
-      justified
-      >
-        <NavItem>
-          <NavLink active href="#">Buy</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Rent</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Sell</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Home Loans</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Agent Finder</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="zillowlogo" href="#"><img src={logo} alt="Zillow Icon" width="180px" /></NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Manage Rentals</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Advertise</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Help</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Sign In</NavLink>
-        </NavItem>
-      </Nav> */}
+      </div>
     </div>
   );
 }
